2aa5fbeff3c00fc0aa0aad885290692d642e8c90
Victor O <viiktechknow@gmail.com>
2023-06-15 23:19:56 +0100

    hello world

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+  * main - entry point to a C program that prints a line with
+  * puts() function as opposed to printf()
+  *
+  * Return: 0 (Success)
+  */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
