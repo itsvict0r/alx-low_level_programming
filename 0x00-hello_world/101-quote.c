f80d7f41acd2049f25cba85e1c9b9eb4f469aa49
Victor O <viiktechknow@gmail.com>
2023-06-16 00:43:11 +0100

    hello world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
+#include <stdio.h>
 #include <unistd.h>
 #include <string.h>
 /**
   * main - entry for program that prints info to standard error
   * without using man(3) printf or man(3) puts
   *
-  * return: 1 (success)
+  * Return: 1 (success)
   */
-int main()
+int main(void)
 {
 	const char *information = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	write(2, information, strlen(information));
--- 0x00-hello_world/size.c
+++ 0x00-hello_world/size.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
-
+/**
+ * main - entry point to program which tells the sizes of data types
+ * 
+ * Return: 0 (success)
+ */
 int main(void)
 {
-	int i;
-	float f;
 	char c;
-	double d;
-
-	printf("Size of int is: %lu bytes.\n",sizeof(i));
-	printf("Size of float is: %lu bytes.\n",sizeof(f));
-	printf("Size of char is: %lu byte.\n",sizeof(c));
-	printf("Size of double is: %lu bytes.\n",sizeof(d));
-
+	int i;
+	long int num;
+	long long int huge;
+	float f;
+	
+	printf("Size of a char: %lu byte(s)\n",sizeof(c));
+	printf("Size of an int: %lu byte(s)\n",sizeof(i));
+	printf("Size of a long int: %lu byte(s)\n",sizeof(num));
+	printf("Size of a long long int: %lu byte(s)\n",sizeof(huge));
+	printf("Size of a float: %lu byte(s)\n",sizeof(f));
 	return (0);
-}	
+}
