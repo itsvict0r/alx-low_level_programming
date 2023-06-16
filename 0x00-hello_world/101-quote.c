1e34b1787ba48e0f118dd10b5d9b3ac1fecebcc5
Victor O <viiktechknow@gmail.com>
2023-06-16 01:12:12 +0100

    hello world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,7 +10,9 @@
 int main(void)
 {
 	const char *i = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	const char *j = "\n";
 
 	write(2, i, strlen(i));
+	write(2, j, strlen(j));
 	return (1);
 }
