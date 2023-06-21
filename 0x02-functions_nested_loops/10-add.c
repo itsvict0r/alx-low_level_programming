3516d445e143c82996a582c93d53733734b74366
Victor O <viiktechknow@gmail.com>
2023-06-21 22:04:22 +0100

    functions

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,11 +2,14 @@
 
 /**
   * add - function adds two numbers
+  * @a: integer argument for function
+  * @b: integer argument for function
   *
   * Return: sum
   */
 int add(int a, int b)
 {
 	int sum = a + b;
+
 	return (sum);
 }
